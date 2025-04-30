b111c25e38c57ba82f256bb07afeba70e79a5f8b
Rocksnotch <williamyouse8@gmail.com>
2025-04-30 01:40:04 -0400

    Cog testing
    testing out how slash commands work, tested with common Ping! command

--- bot.py
+++ bot.py
@@ -1,24 +1,32 @@
 import os
 from dotenv import load_dotenv
+import asyncio
 import discord
 from discord.ext import commands
 from discord import app_commands
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
-
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'We have logged in as {self.user}')
-
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-
-
+DEV_GUILD = os.getenv('DEV_GUILD')
 
 intents = discord.Intents.default()
 intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-client = Client(intents=intents)
-client.run(TOKEN)
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+    await bot.tree.sync()
+
+async def main():
+    async with bot:
+        bot.setup_hook = load_cogs
+        await bot.start(TOKEN)
+
+asyncio.run(main())
---
+++ cogs/testcog.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+class Testcog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'{self.__class__.__name__} is online.')
+
+    @app_commands.command(description='Check the bot\'s latency in ms')
+    async def ping(self, ctx):
+        ping_embed = discord.Embed(
+            title='Pong!',
+            description="Latency in ms",
+            color=discord.Color.blue()
+        )
+        ping_embed.add_field(name=f'{self.bot.user.name}\'s Latency (ms)', value=f'{round(self.bot.latency * 1000)}ms', inline=False)
+        ping_embed.set_footer(text=f'Command invoked by {ctx.user.name}', icon_url=ctx.user.avatar)
+        await ctx.response.send_message(embed=ping_embed)
+
+async def setup(bot):
+    await bot.add_cog(Testcog(bot))
